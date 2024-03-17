c10c8f90c577ccdd8cfdfb9b37000d5952488c53
alahirajeffrey <alahirajeffrey@gmail.com>
2024-03-17 12:41:45 +0100

    setup express app

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require("express")
+const app = express()
+
+const PORT = process.env.port || 3000
+
+app.get("/", (req, res)=>{
+   res.send("Express app runing with nginx as reverese proxy")
+})
+
+app.listen(PORT,()=>{
+   console.log(`Server running on port ${PORT}`)
+})
