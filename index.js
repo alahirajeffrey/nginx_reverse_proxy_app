0c46307d5a344f27b1074a1002c177ebd17d48cb
alahirajeffrey <alahirajeffrey@gmail.com>
2024-03-17 12:58:46 +0100

    commit

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-const express = require("express")
-const app = express()
+const express = require("express");
+const app = express();
 
-const PORT = process.env.port || 3000
+const PORT = process.env.port || 3000;
 
-app.get("/", (req, res)=>{
-   res.send("Express app runing with nginx as reverese proxy")
-})
+app.get("/", (req, res) => {
+  res.send("Express app runing with nginx as reverese proxy");
+});
 
-app.listen(PORT,()=>{
-   console.log(`Server running on port ${PORT}`)
-})
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
